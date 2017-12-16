4f2fef5fcad7fb665d4d941fe3ca2ff698f11d40
devsdmf <devsdmf@gmail.com>
2017-12-16 16:05:12 -0200

    added base correios class

---
+++ correios/correios.py
@@ -0,0 +1,7 @@
+
+# -*- coding: utf-8 -*-
+
+class Correios(object):
+
+    def __init__(self):
+        pass
---
+++ tests/test_correios.py
@@ -0,0 +1,10 @@
+
+# -*- coding: utf-8 -*-
+
+import unittest
+from correios.correios import Correios
+
+class TestCorreios(unittest.TestCase):
+
+    def setUp(self):
+        pass
