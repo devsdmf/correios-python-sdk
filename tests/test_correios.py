ffd52f3c32b82a7c8d0c20cb9f7f9d5b2aaca5af
devsdmf <devsdmf@gmail.com>
2018-02-14 02:34:00 -0200

    added base unit test

--- tests/test_correios.py
+++ tests/test_correios.py
@@ -2,8 +2,12 @@
 
 import unittest
 from correios import Correios
+from correios.package import *
 
 class TestCorreios(unittest.TestCase):
 
     def setUp(self):
+        pass
+    
+    def test_get_shipping_rates(self):
         pass
